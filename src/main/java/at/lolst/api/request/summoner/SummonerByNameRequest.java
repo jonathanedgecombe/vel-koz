d709a3ea62942985742fed48e898010fc1455252
Jonathan Edgecombe <jonathanedgecombe@gmail.com>
2015-07-27 22:07:48 +0100

    Add summoner by name requests.

---
+++ src/main/java/at/lolst/api/request/summoner/SummonerByNameRequest.java
@@ -0,0 +1,68 @@
+package at.lolst.api.request.summoner;
+
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import com.google.gson.reflect.TypeToken;
+
+import at.lolst.api.Util;
+import at.lolst.api.model.Region;
+import at.lolst.api.model.summoner.Summoner;
+import at.lolst.api.request.Request;
+import at.lolst.api.request.RequestAggregator;
+
+/**
+ * 
+ * @author jonathan
+ *
+ */
+public final class SummonerByNameRequest extends Request<Map<String, Summoner>> {
+	private final static Type TYPE = new TypeToken<Map<String, Summoner>>(){}.getType();
+
+	private final List<String> summonerNames;
+
+	public SummonerByNameRequest(Region region, Consumer<Map<String, Summoner>> onCompletion, Consumer<Throwable> onError, String... summonerIds) {
+		super(region, onCompletion, onError);
+		this.summonerNames = Arrays.asList(summonerIds);
+	}
+
+	public List<String> getSummonerNames() {
+		return summonerNames;
+	}
+
+	@Override
+	public String getUrl() {
+		return "/api/lol/" + region + "/v1.4/summoner/by-name/" + Util.join(summonerNames, ",");
+	}
+
+	@Override
+	public Type getType() {
+		return TYPE;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof SummonerByNameRequest)) return false;
+		SummonerByNameRequest r = (SummonerByNameRequest) o;
+		return (r.region == this.region && r.summonerNames.equals(this.summonerNames));
+	}
+
+	@Override
+	public int hashCode() {
+		return (region.hashCode() + summonerNames.hashCode() * 46447);
+	}
+
+	public List<SummonerByNameRequest> explode() {
+		return summonerNames.stream().map(summonerId -> new SummonerByNameRequest(region, onCompletion, onError, summonerId)).collect(Collectors.toList());
+	}
+
+	@Override
+	public Optional<RequestAggregator<Map<String, Summoner>>> getAggregator() {
+		return Optional.of(new SummonerByNameRequestAggregator());
+	}
+}
---
+++ src/main/java/at/lolst/api/request/summoner/SummonerByNameRequestAggregator.java
@@ -0,0 +1,91 @@
+package at.lolst.api.request.summoner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+
+import at.lolst.api.Future;
+import at.lolst.api.model.summoner.Summoner;
+import at.lolst.api.request.Cache;
+import at.lolst.api.request.Request;
+import at.lolst.api.request.RequestAggregator;
+import at.lolst.api.request.RequestDispatcher;
+
+/**
+ * 
+ * @author jonathan
+ *
+ */
+public class SummonerByNameRequestAggregator extends RequestAggregator<Map<String, Summoner>> {
+	@Override
+	public synchronized Future aggregate(Request<Map<String, Summoner>> request, RequestDispatcher dispatcher) throws InterruptedException {
+		Cache cache = dispatcher.getCache();
+
+		BlockingDeque<Request<Map<String, Summoner>>> queue = new LinkedBlockingDeque<>();
+		queue.addAll(request.explode());
+
+		final List<Future> futures = new ArrayList<Future>();
+
+		while (!queue.isEmpty()) {
+			List<String> summonerNames = new ArrayList<>();
+
+			while (summonerNames.size() < 40 && !queue.isEmpty()) {
+				SummonerByNameRequest r = (SummonerByNameRequest) queue.poll();
+
+				synchronized (cache.getExecuting()) {
+					if (cache.isExecuting(r)) {
+						futures.add(cache.onExecute(r));
+						continue;
+					}
+				}
+
+				Optional<Map<String, Summoner>> result = dispatcher.getCache().check(r);
+
+				if (result.isPresent()) {
+					r.getOnCompletion().accept(result.get());
+				} else {
+					summonerNames.add(r.getSummonerNames().get(0));
+				}
+			}
+
+			if (summonerNames.isEmpty()) continue;
+
+			Future future = dispatcher.execute(new SummonerByNameRequest(request.getRegion(), request.getOnCompletion(), request.getOnError(), summonerNames.toArray(new String[0])), false);
+			futures.add(future);
+
+			summonerNames.forEach(summonerName -> {
+				synchronized (cache.getExecuting()) {
+					cache.startExecuting(new SummonerByNameRequest(request.getRegion(), request.getOnCompletion(), request.getOnError(), summonerName), future);
+				}
+			});
+		}
+
+		return new Future() {
+			@Override
+			public void await() throws InterruptedException {
+				for (Future future : futures) future.await();
+			}
+		};
+	}
+
+	@Override
+	public void cache(Request<Map<String, Summoner>> request, final Map<String, Summoner> result, Cache cache) {
+		((SummonerByNameRequest) request).explode().forEach(r -> {
+			String summonerName = r.getSummonerNames().get(0);
+
+			String key = summonerName.toLowerCase().replace(" ", "");
+			Map<String, Summoner> map = new HashMap<>();
+			map.put(key, result.get(key));
+			cache.cache(r, map, false);
+
+			synchronized (cache.getExecuting()) {
+				cache.finishExecuting(new SummonerByNameRequest(request.getRegion(), request.getOnCompletion(), request.getOnError(), summonerName), map);
+			}
+		});
+	}
+}
