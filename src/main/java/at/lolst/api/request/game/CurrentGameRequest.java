426a68009e548d9d41156fed09196dd86fece772
Jonathan Edgecombe <jonathanedgecombe@gmail.com>
2015-08-23 13:31:52 +0100

    Add current game request.

---
+++ src/main/java/at/lolst/api/request/game/CurrentGameRequest.java
@@ -0,0 +1,43 @@
+package at.lolst.api.request.game;
+
+import java.lang.reflect.Type;
+import java.util.function.Consumer;
+
+import com.google.gson.reflect.TypeToken;
+
+import at.lolst.api.model.Region;
+import at.lolst.api.model.currentgame.CurrentGameInfo;
+import at.lolst.api.request.Request;
+
+public final class CurrentGameRequest extends Request<CurrentGameInfo> {
+	private final static Type TYPE = new TypeToken<CurrentGameInfo>(){}.getType();
+
+	private final long summonerId;
+
+	public CurrentGameRequest(Region region, Consumer<CurrentGameInfo> onCompletion, Consumer<Throwable> onError, long summonerId) {
+		super(region, onCompletion, onError);
+		this.summonerId = summonerId;
+	}
+
+	@Override
+	public Type getType() {
+		return TYPE;
+	}
+
+	@Override
+	public String getUrl() {
+		return "/observer-mode/rest/consumer/getSpectatorGameInfo/" + region.getPlatformId() + "/" + summonerId;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof CurrentGameRequest)) return false;
+		CurrentGameRequest r = (CurrentGameRequest) o;
+		return (r.region == region && r.summonerId == summonerId);
+	}
+
+	@Override
+	public int hashCode() {
+		return (int) (region.hashCode() + summonerId * 46447);
+	}
+}
