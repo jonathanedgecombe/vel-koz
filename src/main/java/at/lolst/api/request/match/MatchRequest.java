3985c161d5c951d64c9e98f00c264195abf3debb
Jonathan Edgecombe <jonathanedgecombe@gmail.com>
2015-08-23 02:19:24 +0100

    Added match request.

---
+++ src/main/java/at/lolst/api/request/match/MatchRequest.java
@@ -0,0 +1,45 @@
+package at.lolst.api.request.match;
+
+import java.lang.reflect.Type;
+import java.util.function.Consumer;
+
+import com.google.gson.reflect.TypeToken;
+
+import at.lolst.api.model.Region;
+import at.lolst.api.model.match.Match;
+import at.lolst.api.request.Request;
+
+public final class MatchRequest extends Request<Match> {
+	private final static Type TYPE = new TypeToken<Match>(){}.getType();
+
+	private final long matchId;
+	private final boolean includeTimeline;
+
+	public MatchRequest(Region region, Consumer<Match> onCompletion, Consumer<Throwable> onError, long matchId, boolean includeTimeline) {
+		super(region, onCompletion, onError);
+		this.matchId = matchId;
+		this.includeTimeline = includeTimeline;
+	}
+
+	@Override
+	public Type getType() {
+		return TYPE;
+	}
+
+	@Override
+	public String getUrl() {
+		return "/api/lol/" + region + "/v2.2/match/" + matchId + "?includeTimeline=" + includeTimeline;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof MatchRequest)) return false;
+		MatchRequest r = (MatchRequest) o;
+		return (r.region == region && r.matchId == matchId && r.includeTimeline == includeTimeline);
+	}
+
+	@Override
+	public int hashCode() {
+		return (int) (region.hashCode() + matchId * 46447 + (includeTimeline ? 23 : 7));
+	}
+}
